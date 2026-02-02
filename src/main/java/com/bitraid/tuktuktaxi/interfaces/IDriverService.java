00fc424f55a0715738d213222195715bac71b6f2
Cesar <u20231e202@upc.edu.pe>
2026-02-02 16:29:32 -0500

    Creación de Interface en entidad Driver

--- src/main/java/com/bitraid/tuktuktaxi/interfaces/IDriverService.java
+++ src/main/java/com/bitraid/tuktuktaxi/interfaces/IDriverService.java
@@ -1,5 +1,13 @@
 package com.bitraid.tuktuktaxi.interfaces;
 
-public interface IDriverService {
+import com.bitraid.tuktuktaxi.dtos.DriverDTO;
 
+import java.util.List;
+
+public interface IDriverService {
+    public DriverDTO createDriver(DriverDTO driverDTO);
+    public DriverDTO updateDriver(DriverDTO driverDTO);
+    public Integer deleteDriver(Integer id);
+    public List<DriverDTO> getAllDrivers();
+    public DriverDTO getDriverById(Integer id);
 }
