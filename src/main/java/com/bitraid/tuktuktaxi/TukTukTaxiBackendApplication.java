4fd327f0e22500653f38f67b0ca5efee7384a396
Fabrz18 <fabriziogarrogarcia@gmail.com>
2026-02-02 15:53:13 -0500

    Initial commit

---
+++ src/main/java/com/bitraid/tuktuktaxi/TukTukTaxiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bitraid.tuktuktaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TukTukTaxiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TukTukTaxiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bitraid/tuktuktaxi/TukTukTaxiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitraid.tuktuktaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukTukTaxiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
